e07cd0659494f4ff1740c8be0a5c43afa1c07fb8
Lars Brümmer <lars.bruemmer@virtual-identity.com>
2022-01-06 11:00:40 +0100

    cka-lsg
    - added cors filter

---
+++ src/main/java/de/inmediasp/graphql/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package de.inmediasp.graphql.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600);
+    }
+}
